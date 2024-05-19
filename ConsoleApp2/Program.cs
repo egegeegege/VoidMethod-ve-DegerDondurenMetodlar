715b208bcdb5035dacf95126c9cab309fd817d78
202-12 <202-12@202-12>
2024-05-19 11:47:37 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1. sayıyı giriniz");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. sayıyı giriniz");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            Kullanicilar(sayi1,sayi2);
+        }
+        static void Kullanicilar(int s1,int s2)
+        {
+            if (s1 < s2)
+            {
+                Console.WriteLine("girdiğiniz 1. sayı 2. sayıdan küçük");
+            }
+            else if (s1 > s2)
+            {
+                Console.WriteLine("girdiğiniz 2. sayı 1. sayıdan küçük");
+            }
+            else
+            {
+                Console.WriteLine("girdiğiniz iki sayıda birbirine eşit");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
---
+++ ConsoleAppMetodt2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMetodt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. sayıyı giriniz");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. sayıyı giriniz");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FarkAl(sayi1, sayi2));
+            Console.WriteLine(ToplamAl(sayi1, sayi2));
+
+            Console.ReadLine();
+        }
+
+        static int FarkAl(int s1, int s2)
+        {
+            int sonuc = s1 - s2;
+            Console.WriteLine(sonuc);
+            return sonuc;
+        }
+
+        static int ToplamAl(int s1, int s2)
+        {
+            int sonuc = s1 + s2;
+            Console.WriteLine(sonuc);
+            return sonuc;
+        }
+    }
+}
